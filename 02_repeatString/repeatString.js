6da382b7f0f7bf776eca8b600aa59c32942c29ea
dlanik <lanik.dominik@gmail.com>
2024-09-22 21:31:25 +0200

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  let resultStr = '';
+  for (let i = 0; i < num; i++) {
+    resultStr += str;
+  }
 
+  return resultStr;
 };
 
 // Do not edit below this line
