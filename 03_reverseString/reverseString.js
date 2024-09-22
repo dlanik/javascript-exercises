f05b6f234045364feb891388fa08dba5643e8d36
dlanik <lanik.dominik@gmail.com>
2024-09-22 21:35:03 +0200

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let splitInput = input.split('');
+  splitInput.reverse();
+  return splitInput.join('');
 };
 
 // Do not edit below this line
